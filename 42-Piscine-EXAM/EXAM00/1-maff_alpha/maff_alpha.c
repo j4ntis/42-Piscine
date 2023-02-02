54a993ffe0061a101d0609255d851455d861a5fe
janties <com.kerem@gmail.com>
2023-02-03 02:36:08 +0300

    besincii

--- 42-Piscine-EXAM/EXAM00/1-maff_alpha/maff_alpha.c
+++ 42-Piscine-EXAM/EXAM00/1-maff_alpha/maff_alpha.c
@@ -1,29 +1,24 @@
 #include <unistd.h>
 
-void maff_alpha(void)
-{
-	char letter = 'a';
-	while(letter <= 'z')
-	{
-		if (letter % 2 == 0)//if letter is 
-		{
-			letter -= 32;
-			write(1, &letter, 1);
-			letter += 32;
-		}
-		else
-		{
-			write(1, &letter, 1);
-		}
-		letter +=1;
-	}
-	write(1,"\\n", 2);
-}
-/*
 int main()
 {
-	maff_alpha();
+        char letter = 'a';
+        while(letter <= 'z')
+        {
+                if (letter % 2 == 0)//if letter is 
+                {
+                        letter -= 32;
+                        write(1, &letter, 1);
+                        letter += 32;
+                }
+                else
+                {
+                        write(1, &letter, 1);
+                }
+                letter +=1;
+        }
+        write(1,"\\n", 2);
 	return 0;
 }
-*/
+
 
