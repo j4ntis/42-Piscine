fc3f0d97a985a3963f0aa070f4cef36799554b36
kisik-42 <80246006+kisik-42@users.noreply.github.com>
2023-02-20 02:17:21 +0300

    Add files via upload

--- EXAMS/EXAM02/ft_strdup/ft_strdup.c
+++ EXAMS/EXAM02/ft_strdup/ft_strdup.c
@@ -1,14 +1,24 @@
 #include <stdlib.h>
-char *ft_strdup(char *src)
+char *ft_strdup(char *s1)
 {
-	int len = 0;
-	while(src[len]) len++;
-	char *ret = malloc(sizeof(char) * (len + 1));
-	len = 0;
-	while (src[len])
-	{
-		ret[len] = src[len];
-		len++;
-	}
-	return ret;
+    int len = 0;
+    while(s1[len]){len++;}
+
+    char *s2 = malloc(sizeof(char) * (len+1));
+    int i = 0;
+    while (i<=len)
+    {
+        s2[i] = s1[i];
+        i++;
+    }
+    return s2;
 }
+/*
+#include <stdio.h>
+int main()
+{
+    char a[] = "kerem";
+    printf("%s", ft_strdup(a));
+}
+*/
+
